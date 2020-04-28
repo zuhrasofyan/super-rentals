04a8bfb67caa2fce9ebacb5760cc4f159dba88d8
Zuhra <zuhrasofyan@gmail.com>
2020-04-28 16:45:38 +0200

    Update list-rentals-test.js

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,10 +4,24 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should show rentals as the homepage', function (assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically')
+  })
 });
 test('should link to information about company', function (assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  })
 });
 test('should link to contact information', function (assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  })
 });
 test('should list available rentals', function (assert){
 });
