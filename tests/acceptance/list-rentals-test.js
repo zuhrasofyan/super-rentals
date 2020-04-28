d29ac268144e6d2567fe27209513b9cc7c96a5f0
Zuhra <zuhrasofyan@gmail.com>
2020-04-28 15:52:36 +0200

    test for later purpose

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,10 +3,15 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('visiting /', function(assert) {
-  visit('/');
-
-  andThen(function() {
-    assert.equal(currentURL(), '/');
-  });
+test('should show rentals as the homepage', function (assert){
+});
+test('should link to information about company', function (assert){
+});
+test('should link to contact information', function (assert){
+});
+test('should list available rentals', function (assert){
+});
+test('should filter available rentals by city', function (assert){
+});
+test('should show details for a selected rental', function (assert){
 });
